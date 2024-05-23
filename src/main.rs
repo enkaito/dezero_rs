ed159078b4552732d35133d70791dde02d06d55e
enkaito <enkaito@gmail.com>
2024-05-23 22:37:12 +0900

    finally I won! (partially)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use core::panic;
-use std::{cell::RefCell, fmt::Debug, rc::Rc};
+use std::{
+    cell::RefCell,
+    fmt::Debug,
+    rc::{Rc, Weak},
+};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Variable {
     data: f32,
     grad: Option<f32>,
-    creator: Option<Rc<dyn Function>>,
+    creator: Option<Function>,
 }
 
 #[derive(Clone, Debug)]
@@ -16,14 +19,67 @@ impl VBox {
         VBox(Rc::new(RefCell::new(Variable::new(data))))
     }
 
-    fn set_creator(&self, creator: Rc<dyn Function>) {
+    fn get_data(&self) -> f32 {
         let v = self.0.as_ref();
-        v.borrow_mut().creator = Some(creator);
+        v.borrow().data
+    }
+
+    fn get_grad(&self) -> f32 {
+        let v = self.0.as_ref();
+        v.borrow().grad.unwrap()
+    }
+
+    fn get_option_grad(&self) -> Option<f32> {
+        let v = self.0.as_ref();
+        v.borrow().grad
+    }
+
+    fn get_creator(&self) -> Option<Function> {
+        self.0.clone().borrow().creator.clone()
     }
 
     fn set_grad(&self, grad: f32) {
         let v = self.0.as_ref();
         v.borrow_mut().grad = Some(grad);
+    }
+
+    fn set_creator(&self, creator: Function) {
+        let v = self.0.as_ref();
+        v.borrow_mut().creator = Some(creator);
+    }
+
+    fn backward(&self) {
+        if self.get_option_grad().is_none() {
+            self.set_grad(1.);
+        }
+
+        let mut funcs = vec![self.get_creator().unwrap()];
+        while let Some(f) = funcs.pop() {
+            let x = f.input.clone().unwrap();
+            let y = f.output.clone().unwrap();
+
+            x.set_grad(f.backward(y.get_grad()));
+
+            if let Some(x_creator) = x.get_creator() {
+                funcs.push(x_creator)
+            };
+        }
+    }
+
+    fn downgrade(self) -> WeakVBox {
+        WeakVBox(Rc::downgrade(&self.0))
+    }
+}
+
+#[derive(Debug, Clone)]
+struct WeakVBox(Weak<RefCell<Variable>>);
+
+impl WeakVBox {
+    fn get_grad(&self) -> f32 {
+        let tmp = self.0.upgrade().unwrap();
+        let v = tmp.as_ref();
+        let x = v.borrow().grad.unwrap();
+        x
     }
 }
 
@@ -37,117 +93,74 @@ impl Variable {
     }
 }
 
-trait Function: Debug {
-    fn call(&mut self, input: VBox) -> VBox {
-        let x = input.0.borrow().data;
+#[derive(Debug, Clone)]
+enum FType {
+    Square,
+    Exp,
+}
+
+#[derive(Debug, Clone)]
+struct Function {
+    input: Option<VBox>,
+    output: Option<WeakVBox>,
+    ftype: FType,
+}
+
+impl Function {
+    fn new(ftype: FType) -> Function {
+        Function {
+            input: None,
+            output: None,
+            ftype: ftype,
+        }
+    }
+
+    fn call(mut self, input: &VBox) -> VBox {
+        let x = input.get_data();
         let y = self.forward(x);
         let output = VBox::new(y);
+        self.input = Some(input.clone());
+        self.output = Some(output.clone().downgrade());
         output.set_creator(self);
-        self.set(&input, &output);
         output
     }
 
-    fn new() -> Self
-    where
-        Self: Sized;
-    fn input(&self) -> VBox;
-    fn set(&mut self, input: &VBox, output: &VBox);
-    fn forward(&self, x: f32) -> f32;
-    fn backward(&self, gy: f32) -> f32;
-}
-
-#[derive(Debug)]
-struct Square {
-    input: Option<VBox>,
-    output: Option<VBox>,
-}
-
-impl Function for Square {
-    fn new() -> Self {
-        Square {
-            input: None,
-            output: None,
+    fn forward(&self, x: f32) -> f32 {
+        match &self.ftype {
+            FType::Square => x.powi(2),
+            FType::Exp => x.exp(),
         }
     }
 
-    fn input(&self) -> VBox {
-        let Some(input) = &self.input else {
-            panic!("input is not set")
-        };
-        input.clone()
-    }
-
-    fn set(&mut self, input: &VBox, output: &VBox) {
-        self.input = Some(input.clone());
-        self.output = Some(output.clone());
-    }
-
-    fn forward(&self, x: f32) -> f32 {
-        x.powi(2)
-    }
-
     fn backward(&self, gy: f32) -> f32 {
-        let input = self.input();
-        let x = input.0.borrow().data;
-        2. * x * gy
+        let x = self.input.as_ref().unwrap().get_data();
+        let pdv = match &self.ftype {
+            FType::Square => 2. * x,
+            FType::Exp => x.exp(),
+        };
+        pdv * gy
     }
 }
 
-#[derive(Debug)]
-struct Exp {
-    input: Option<VBox>,
-    output: Option<VBox>,
+macro_rules! square {
+    ($x: expr) => {{
+        let func = Function::new(FType::Square);
+        func.call(&$x)
+    }};
 }
 
-impl Function for Exp {
-    fn new() -> Self {
-        Exp {
-            input: None,
-            output: None,
-        }
-    }
-
-    fn input(&self) -> VBox {
-        let Some(input) = &self.input else {
-            panic!("input is not set")
-        };
-        input.clone()
-    }
-
-    fn set(&mut self, input: &VBox, output: &VBox) {
-        self.input = Some(input.clone());
-        self.output = Some(output.clone());
-    }
-
-    fn forward(&self, x: f32) -> f32 {
-        x.exp()
-    }
-
-    fn backward(&self, gy: f32) -> f32 {
-        let input = self.input();
-        let x = input.0.borrow().data;
-        x.exp() * gy
-    }
+macro_rules! exp {
+    ($x: expr) => {{
+        let func = Function::new(FType::Exp);
+        func.call(&$x)
+    }};
 }
 
 fn main() {
-    let mut A = Square::new();
-    let mut B = Exp::new();
-    let mut C = Square::new();
-
     let x = VBox::new(0.5);
-    println!("{x:?}");
-    let a = A.call(x.clone());
-    println!("{a:?}");
-    let b = B.call(a.clone());
-    println!("{b:?}");
-    let y = C.call(b.clone());
-    println!("{y:?}");
 
-    y.set_grad(1.);
-    b.set_grad(C.backward(y.0.borrow().grad.unwrap()));
-    a.set_grad(B.backward(b.0.borrow().grad.unwrap()));
-    x.set_grad(A.backward(a.0.borrow().grad.unwrap()));
+    let y = square!(exp!(square!(x)));
 
-    println!("{x:?}");
+    y.backward();
+    println!("{}", x.get_grad())
 }
