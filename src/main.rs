909d35208c2261d4e4ada61296acd2d22183032d
enkaito <enkaito@gmail.com>
2024-05-23 20:49:51 +0900

    stack on setting creator to the variables

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
+use core::panic;
+use std::{cell::RefCell, fmt::Debug, rc::Rc};
+
+#[derive(Debug)]
 struct Variable {
     data: f32,
     grad: Option<f32>,
-    creator: Option<Box<dyn Function>>,
+    creator: Option<Rc<dyn Function>>,
+}
+
+#[derive(Clone, Debug)]
+struct VBox(Rc<RefCell<Variable>>);
+
+impl VBox {
+    fn new(data: f32) -> VBox {
+        VBox(Rc::new(RefCell::new(Variable::new(data))))
+    }
+
+    fn set_creator(&self, creator: Rc<dyn Function>) {
+        let v = self.0.as_ref();
+        v.borrow_mut().creator = Some(creator);
+    }
+
+    fn set_grad(&self, grad: f32) {
+        let v = self.0.as_ref();
+        v.borrow_mut().grad = Some(grad);
+    }
 }
 
 impl Variable {
@@ -12,26 +35,119 @@ impl Variable {
             creator: None,
         }
     }
+}
 
-    fn set_creator(&mut self, func: impl Function + 'static) {
-        self.creator = Some(Box::new(func));
+trait Function: Debug {
+    fn call(&mut self, input: VBox) -> VBox {
+        let x = input.0.borrow().data;
+        let y = self.forward(x);
+        let output = VBox::new(y);
+        output.set_creator(self);
+        self.set(&input, &output);
+        output
     }
 
-    fn backward(&mut self) {
-        if let Some(f) = self.creator {
-            let x = f.input;
-            x.grad = f.backward(self.grad);
-            x.backward();
+    fn new() -> Self
+    where
+        Self: Sized;
+    fn input(&self) -> VBox;
+    fn set(&mut self, input: &VBox, output: &VBox);
+    fn forward(&self, x: f32) -> f32;
+    fn backward(&self, gy: f32) -> f32;
+}
+
+#[derive(Debug)]
+struct Square {
+    input: Option<VBox>,
+    output: Option<VBox>,
+}
+
+impl Function for Square {
+    fn new() -> Self {
+        Square {
+            input: None,
+            output: None,
+        }
+    }
+
+    fn input(&self) -> VBox {
+        let Some(input) = &self.input else {
+            panic!("input is not set")
         };
+        input.clone()
+    }
+
+    fn set(&mut self, input: &VBox, output: &VBox) {
+        self.input = Some(input.clone());
+        self.output = Some(output.clone());
+    }
+
+    fn forward(&self, x: f32) -> f32 {
+        x.powi(2)
+    }
+
+    fn backward(&self, gy: f32) -> f32 {
+        let input = self.input();
+        let x = input.0.borrow().data;
+        2. * x * gy
     }
 }
 
-trait Function {
-    fn call(&self, input: Variable) -> Variable;
-    fn forward(&self, x: Variable) -> Variable;
-    fn backward(&self, gy: Variable) -> Variable;
+#[derive(Debug)]
+struct Exp {
+    input: Option<VBox>,
+    output: Option<VBox>,
+}
+
+impl Function for Exp {
+    fn new() -> Self {
+        Exp {
+            input: None,
+            output: None,
+        }
+    }
+
+    fn input(&self) -> VBox {
+        let Some(input) = &self.input else {
+            panic!("input is not set")
+        };
+        input.clone()
+    }
+
+    fn set(&mut self, input: &VBox, output: &VBox) {
+        self.input = Some(input.clone());
+        self.output = Some(output.clone());
+    }
+
+    fn forward(&self, x: f32) -> f32 {
+        x.exp()
+    }
+
+    fn backward(&self, gy: f32) -> f32 {
+        let input = self.input();
+        let x = input.0.borrow().data;
+        x.exp() * gy
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut A = Square::new();
+    let mut B = Exp::new();
+    let mut C = Square::new();
+
+    let x = VBox::new(0.5);
+    println!("{x:?}");
+    let a = A.call(x.clone());
+    println!("{a:?}");
+    let b = B.call(a.clone());
+    println!("{b:?}");
+    let y = C.call(b.clone());
+    println!("{y:?}");
+
+    y.set_grad(1.);
+    b.set_grad(C.backward(y.0.borrow().grad.unwrap()));
+    a.set_grad(B.backward(b.0.borrow().grad.unwrap()));
+    x.set_grad(A.backward(a.0.borrow().grad.unwrap()));
+
+    println!("{x:?}");
 }
